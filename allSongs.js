1a59adc9383df127cbcc58013ebbcdc34f53d7db
yordanzhelevdev <yordanzhelevdev@gmail.com>
2020-07-07 00:20:55 +0300

    .

---
+++ allSongs.js
@@ -0,0 +1,12 @@
+exports.songs = [
+    'Skandau-Haivera',
+    'Skandau-Replay',
+    'Skandau i Deo - Ot Yuli do Avgust',
+    'Skandau - Maika mu stara',
+    'Skandau - Ot spomen do rana',
+    'Toto H - Chujdi usmivki',
+    'Skandau - Do moreto',
+    'Skandau feat Charlie - No',
+    'Skandau - Kokaina',
+    'Skandau - Kolio Ficheto'
+];
